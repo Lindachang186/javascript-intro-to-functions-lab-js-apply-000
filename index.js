dd2dbeddaf747f86240a683e0a7be7ace1301d52
Linda Chang <lindachang186@gmail.com>
2018-09-06 15:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var shout(string){
-  string.toUpperCase()
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+sayHiToGrandma(string){
+  if(string === string.toLowerCase()){
+    return "I can't hear you"
+  } else if(string === string.toUpperCase()) {
+    
+  }
 }
